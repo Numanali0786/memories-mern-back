fdb3b97db1433a4357c511b096243c59eca7f5cb
Numan Ali <numan4599322ali@gmail.com>
2023-04-19 16:01:48 +0530

    final

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 // testing
-// app.use(cors());
+app.use(cors());
 
 // deploying
-app.use(
-  cors({
-    origin: "https://memories-react-front.onrender.com",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://memories-react-front.onrender.com",
+//   })
+// );
 
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
